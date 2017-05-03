251753d002a607b4432a4cf696753de20cf54919
rainu <rainu@raysha.de>
2017-05-03 21:26:43 +0000

    fix tests

--- cloud/src/test/java/de/rainu/alexa/cloud/calendar/EventMapperTest.java
+++ cloud/src/test/java/de/rainu/alexa/cloud/calendar/EventMapperTest.java
@@ -7,6 +7,7 @@ import biweekly.component.VEvent;
 import de.rainu.alexa.cloud.calendar.model.Event;
 import java.util.TimeZone;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 public class EventMapperTest {
@@ -17,8 +18,8 @@ public class EventMapperTest {
   public void map() {
     //given
     final VEvent event = new VEvent();
-    event.setDateStart(DateTime.parse("2009-12-15T22:15:00Z").toDate(), true);
-    event.setDateEnd(DateTime.parse("2010-08-13T20:15:00Z").toDate(), true);
+    event.setDateStart(DateTime.parse("2009-12-15T22:15:00").withZone(DateTimeZone.UTC).toDate(), true);
+    event.setDateEnd(DateTime.parse("2010-08-13T20:15:00").withZone(DateTimeZone.UTC).toDate(), true);
     event.setSummary("<summary>");
     event.setDescription("<description>");
 
@@ -28,8 +29,8 @@ public class EventMapperTest {
     final Event result = toTest.map(event, tz);
 
     //then
-    assertEquals(DateTime.parse("2009-12-15T23:15:00.000+01:00").toString(), result.getStart().toString());
-    assertEquals(DateTime.parse("2010-08-13T22:15:00.000+02:00").toString(), result.getEnd().toString());
+    assertEquals(DateTime.parse("2009-12-15T22:15:00").withZone(DateTimeZone.forID("Europe/Berlin")).toString(), result.getStart().toString());
+    assertEquals(DateTime.parse("2010-08-13T20:15:00").withZone(DateTimeZone.forID("Europe/Berlin")).toString(), result.getEnd().toString());
     assertEquals("<summary>", result.getSummary());
     assertEquals("<description>", result.getDescription());
   }
@@ -38,8 +39,8 @@ public class EventMapperTest {
   public void map_noTime() {
     //given
     final VEvent event = new VEvent();
-    event.setDateStart(DateTime.parse("2009-12-15T22:15:00Z").toDate(), false);
-    event.setDateEnd(DateTime.parse("2010-08-13T21:15:00Z").toDate(), false);
+    event.setDateStart(DateTime.parse("2009-12-15T22:15:00").withZone(DateTimeZone.UTC).toDate(), false);
+    event.setDateEnd(DateTime.parse("2010-08-13T21:15:00").withZone(DateTimeZone.UTC).toDate(), false);
     event.setSummary("<summary>");
     event.setDescription("<description>");
 
@@ -49,8 +50,8 @@ public class EventMapperTest {
     final Event result = toTest.map(event, tz);
 
     //then
-    assertEquals(DateTime.parse("2009-12-15T01:00:00.000+01:00").toString(), result.getStart().toString());
-    assertEquals(DateTime.parse("2010-08-13T02:00:00.000+02:00").toString(), result.getEnd().toString());
+    assertEquals(DateTime.parse("2009-12-15T00:00:00").withZone(DateTimeZone.forID("Europe/Berlin")).toString(), result.getStart().toString());
+    assertEquals(DateTime.parse("2010-08-13T00:00:00").withZone(DateTimeZone.forID("Europe/Berlin")).toString(), result.getEnd().toString());
     assertEquals("<summary>", result.getSummary());
     assertEquals("<description>", result.getDescription());
   }
